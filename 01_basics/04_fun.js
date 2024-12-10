7f871051fae12f0987c7856c4fbfa4dc44f02df2
keshab gurung <117005744+keshabg8473@users.noreply.github.com>
2024-12-10 03:45:53 +0000

    function object

--- 01_basics/04_fun.js
+++ 01_basics/04_fun.js
@@ -21,10 +21,25 @@
 // // console.log(userlogin());//when we don't pass the value it print undefind;
 // console.log(userlogin());//when userlogin in empty print defind value but when we pass the argument it overwrite the defind value
 
-//------------------------------...rest operator------
+//------------------------------...rest operator----------------
 
 // jab koi continue value pass karega and ush value ko tum kisibi array main store karna chahoge to rest operator used karna padega
-function CalculateCartPrice(val1, val2, ...num1){
-    return num1;
+// function CalculateCartPrice(val1, val2, ...num1){
+//     return num1;
+// }
+// console.log(CalculateCartPrice(200,30,203,120,240));
+
+// --------------------------------object in function how access and how defind------------
+// let obj = {
+//     name : "keshab",
+//     oquepation: "software development"
+// }
+
+function printObjectValue(passObjectPro){
+    console.log(`myname is ${passObjectPro.name} and i am pasonated about ${passObjectPro.oquepation}`);
 }
-console.log(CalculateCartPrice(200,30,203,120,240));
+// printObjectValue(obj)//this way first create an object and then call it 
+printObjectValue({
+    name : "ariyan",
+    oquepation:"ai expart"
+})
