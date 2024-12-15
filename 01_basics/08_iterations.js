1eeb0a90b6335fb320b1f9febb538bfc7a165cb5
keshab gurung <117005744+keshabg8473@users.noreply.github.com>
2024-12-15 17:25:08 +0000

    loops

--- 01_basics/08_iterations.js
+++ 01_basics/08_iterations.js
@@ -17,4 +17,24 @@
 // for (let index = 0; index < fruits.length; index++) {
 //     let element = fruits[index];
 //     console.log(element);
-// }//nested for loop or 2D array
+// }//nested for loop or 2D array--------------------------
+// for (let i = 1; i < 5; i++) {
+//     // console.log(`outer value ${i}`);
+//     for (let j = 1; j < 5; j++) {
+//         console.log(`inner value ${i}${j}`);
+//     }
+// }//print multiplication 
+// for (let i = 1; i <= 10; i++) {
+//     for (let j = 1; j <= 10; j++) {
+//         console.log(i + " * " + j + " = " + i*j );
+//     }
+// }-------------break and continue statement-------------
+// for (let i = 1; i <= 10; i++) {
+//     if(i == 5){
+//         console.log(`detected value is ${i}`);
+//         // break;
+//         continue;//the continue statement "jumps over" one iteration in the loop.
+//     }
+//     console.log(`value is ${i}`);
+// }
+// ----------------------------------------------------------------------------------------------------------------------------
